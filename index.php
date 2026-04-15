da0c1986c214d46ab55966ce9678d5693f65559a
cursodetionlineoficial1 <cursodetionlineoficial1@gmail.com>
2026-04-15 17:17:06 -0300

    sexta edição

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
+<?php
 
+ include_once "./conexao.php";
+
+?>
 
 <!DOCTYPE html>
 
 <html lang="pt-br">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Página Inicial</title>
-    <meta name="description" content="">
+    <title>Cadastro</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
+    <link href="css/style.css" rel="stylesheet">
   </head>
   <body>
-    <h1>Página Inicial</h1>
-    
+    <h1>Cadastrar</h1>
+    <form action="" method="post">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" placeholder="Digite seu nome" requered>
+
+      <label for="e-mail">E-mail:</label>
+      <input type="email" id="email" placeholder="Digite seu E-mail" requered>
+
+      <label for="usuario">Usuário:</label>
+      <input type="text" id="usuario" placeholder="Digite seu nome de Usuario" requered>
+
+      <label for="senha">Senha:</label>
+      <input type="password" id="senha" placeholder="Digite sua senha" requered>
+
+      <label for="data_nascimento">Data de Nascimento:</label>
+      <input type="date" id="data_nascimento" placeholder="dd/mm/aaaa" requered>
+    </form>
   </body>
 </html>
