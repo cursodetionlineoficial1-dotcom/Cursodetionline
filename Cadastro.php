e89ea475db9a4680075881d1efc272bda3dbba57
cursodetionlineoficial1 <cursodetionlineoficial1@gmail.com>
2026-04-15 16:47:24 -0300

    quinta edição

---
+++ Cadastro.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>Cadastro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Cadastrar</h1>
+    <form action="" method="post">
+      <label for="nome">Nome:</label>
+      <input type="text" id="nome" placeholder="Digite seu nome" requered>
+
+      <label for="e-mail">E-mail:</label>
+      <input type="email" id="email" placeholder="Digite seu E-mail" requered>
+
+      <label for="usuario">Usuário:</label>
+      <input type="text" id="usuario" placeholder="Digite seu nome de Usuario" requered>
+
+      <label for="senha">Senha:</label>
+      <input type="password" id="senha" placeholder="Digite sua senha" requered>
+
+      <label for="data_nascimento">Data de Nascimento:</label>
+      <input type="date" id="data_nascimento" placeholder="dd/mm/aaaa" requered>
+    </form>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,66 +1,18 @@
-<?php
 
-  include_once "conexao.php";
 
-?>
+<!DOCTYPE html>
 
-<!doctype html>
 <html lang="pt-br">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Cadastro</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-
-<body>
-  <div class="container">
-    <div class="row g-3 align-items-center justify-content-center mt-3">
-      <div class="col">
-        <h1 style="margin-bottom: 20px; margin-top: 20px;">Cadastro</h1>
-        <form class="row g-2" action="cadastro_script.php" method="POST">
-          <div class="form-floating mb-2">
-            <input type="text" class="form-control" id="floatingInput">
-            <label for="floatingInput">Digite seu Nome Completo</label>
-          </div>
-
-          <div class="form-floating mb-2">
-            <input type="text" class="form-control" id="floatingInput">
-            <label for="floatingInput">Digite seu Endereço:</label>
-          </div>
-
-          <div class="form-floating mb-3">
-            <label for="floatingInput">Digite seu Telefone:</label>
-            <input type="text" class="form-control" id="floatingInput">
-          </div>
-          
-          <div class="form-floating mb-2">
-            <input type="email" class="form-control" id="floatingEmail">
-            <label for="floatingEmail">Digite seu E-mail:</label>
-          </div>
-
-          <div class="form-floating mb-2">
-            <input type="date" class="form-control" id="floatingDate">
-            <label for="floatingDate">Digite sua Data de Nascimento:</label>
-          </div>
-
-          <div class="col-12 mb-2">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-              <label class="form-check-label" for="invalidCheck2">
-                Ao clicar nesta caixa, você estará aceitando nossos <a href="">Termos de Uso</a> e os <a href="">Termos de Privacidade</a> do sistema de cadastro do Curso de TI Online. Isto esta de acordo com as Leis 12.965/2014 e 13.709/2018, que regulam o uso da Internet e o tratamento de dados pessoais no Brasil.
-              </label>
-            </div>
-          </div>
-          <div class="col-12 mb-3">
-          <button type="button" class="btn btn-outline-success">Salvar</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
-
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Página Inicial</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+  </head>
+  <body>
+    <h1>Página Inicial</h1>
+    
+  </body>
 </html>
